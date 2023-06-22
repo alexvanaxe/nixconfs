860a26de755a6707e15975f5f57e6374d6b51420
AlexVanAxe <nightwalker107@gmail.com>
2023-06-22 14:22:23 -0300

    Update configuration.nix

--- nixconfs/configuration.nix
+++ nixconfs/configuration.nix
@@ -39,7 +39,8 @@
   # };
 
   # Enable the X11 windowing system.
-  # services.xserver.enable = true;
+   services.xserver.enable = true;
+   services.xserver.displayManager.startx.enable = true;
 
 
   
